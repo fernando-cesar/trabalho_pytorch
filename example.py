e12362b29cbc0b668f63ebae958572ddc04754a8
fernando-cesar <55638654+fernando-cesar@users.noreply.github.com>
2022-03-01 15:02:24 -0300

    atualizando arquivo exemplo

--- example.py
+++ example.py
@@ -4,4 +4,26 @@ import torch
 
 # print(torch.full((2, 3), 3.141592))
 
-print(torch.empty((2, 3)))
+# print(torch.empty((2, 3)))
+
+# valores aleatorios a partir de uma distribuição normal entre 0 e 1
+# print(torch.rand((2, 3)))
+
+# valores aleatorios com media 0 e variancia 1 de uma distribuição normal
+# print(torch.randn((2, 3)))
+
+# print(torch.randint(10, 100, (2, 3)))
+
+# print(torch.tensor([[1, 2, 3], [4, 5, 6]]))
+
+# criando tensor a partir de outro tensor
+a = torch.tensor([[1, 2, 3], [4, 5, 6]])
+print(a.dtype)
+print(a.shape)
+print('\n')
+b = torch.ones_like(a)
+print(b)
+print(b.dtype)
+print(b.shape)
+print('\n')
+print(a.new_full((2, 2), 3.0))
